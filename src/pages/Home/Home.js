f9fcafb05af758dac804cadf884244fd2d2eb0e8
jacob-bon <jboner2111@gmail.com>
2025-04-14 21:21:03 +0200

    feat:Log out function

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,27 @@
-// import { useState } from 'react';
-// import { signInWithEmailAndPassword } from 'firebase/auth';
-// import { auth } from '../../firebase';
-// import { useNavigate } from 'react-router-dom';
-// import { Link } from 'react-router-dom'; 
+import React from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
 import '../../App.css';
 
 const Home = () => {
+  const navigate = useNavigate();
 
-return (
-    <div className = "login-wrapper">
-<h1>This will be the home page</h1>
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate('/login');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
 
+  return (
+    <div className="login-wrapper">
+      <h1>This will be the home page</h1>
+      <button className = "logout-button" onClick={handleLogout}>Log Out</button>
     </div>
-
-
-);
-
+  );
 };
 
 export default Home;
