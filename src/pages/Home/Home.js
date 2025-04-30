0677b3ae414c0111e1bb1eabbb0dac36fd110639
Aharon Zagnoev <aharondzagnoev@gmail.com>
2025-05-01 00:17:42 +0200

    Added a search functionality for buyer in Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,8 @@ import ProductCard from './ProductCard';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  //new
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -19,12 +21,32 @@ const Home = () => {
     fetchProducts();
   }, []);
 
+  products.forEach((product) => {
+    console.log("Product name:", product.name);
+  });
+
+
+  const filteredProducts = products.filter(product =>
+    product.name?.toLowerCase().trim().includes(searchTerm.toLowerCase().trim())
+  );
+  
+  
+
   return (
     <div style={styles.container}>
       <h1 style={styles.heading}>Explore Products</h1>
+      
+      <input
+        type="text"
+        placeholder="Search for a product..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={styles.searchInput}
+      />
+
       <div style={styles.productGrid}>
-        {products.length > 0 ? (
-          products.map(product => (
+      {filteredProducts.length > 0 ? (
+        filteredProducts.map(product => (
             <ProductCard key={product.id} product={product} />
           ))
         ) : (
@@ -50,6 +72,16 @@ const styles = {
     fontWeight: 'bold',
     marginBottom: 24,
     color: '#3b82f6',
+  },
+  //new
+  searchInput: {
+    padding: '12px 16px',
+    borderRadius: 8,
+    border: '1px solid #ccc',
+    marginBottom: 24,
+    width: '100%',
+    maxWidth: 400,
+    fontSize: 16,
   },
   productGrid: {
     display: 'flex',
